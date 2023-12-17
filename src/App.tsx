25995dcf4263b1f3bb23c77ce581f2cbc4af273c
koykoy027 <villanuevajoshua27@gmail.com>
2023-12-17 20:49:37 +0800

    install da-tables components

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,27 @@
 import { useQuery } from 'react-query';
 import { fetchStudents } from './lib/apis/students';
+import { columns } from "./lib/columns/students"
+import { DataTable } from "./components/data-table"
+import { Skeleton } from "@/components/ui/skeleton"
+
+
 
 function App() {
   const { data, isLoading, isError } = useQuery('students', fetchStudents);
 
+
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <><h1>Loading...</h1></>;
   }
 
   if (isError) {
-    return <p>Error fetching data</p>;
+    return <p>Something went wrong {isError}</p>;
   }
   return (
-    <div className="container border">
-      <h1 className="uppercase font-bold">Fetch apis from laravel localhost</h1>
-      <hr />
-
-      <table className="w-full bg-white border border-gray-200 shadow-md rounded-md overflow-hidden">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="py-2 px-4 border-b">ID</th>
-            <th className="py-2 px-4 border-b">Name</th>
-            <th className="py-2 px-4 border-b">Email</th>
-            <th className="py-2 px-4 border-b">Gender</th>
-          </tr>
-        </thead>
-
-        <tbody>
-        {data.map((student) => (
-          <tr key={student.id} className="hover:bg-gray-50">
-            <td className="py-2 px-4 border-b">{student.id}</td>
-            <td className="py-2 px-4 border-b">{student.name}</td>
-            <td className="py-2 px-4 border-b">{student.email}</td>
-            <td className="py-2 px-4 border-b">{student.gender}</td>
-        </tr>
-        ))}
-
-          
-        </tbody>
-      </table>
+    <div className="container border p-10">      
+      <h1 className="uppercase font-bold mt-10">using Shadcn/ui Datatables</h1>
+      <DataTable columns={columns} data={data} />
+      
     </div>
   );
 }
---
+++ src/lib/columns/students.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Button } from "../../components/ui/button";
+
+export type Payment = {
+  id: string;
+  name: string;
+  amount: number;
+  gender: "Male" | "Female";
+  email: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "gender",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Gender
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+];
